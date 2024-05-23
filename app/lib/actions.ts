60c21163eb682631e10699c1a343f8f05338d66a
AgustinLeguizamon <agustinmleguizamon@gmail.com>
2024-05-23 12:03:35 -0300

    se termina capitulo 12

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,8 @@
 
 import { z } from 'zod';
 import {sql} from "@vercel/postgres";
+import {revalidatePath} from "next/cache";
+import {redirect} from "next/navigation";
 
 const FormSchema = z.object({
   id: z.string(),
@@ -24,6 +26,36 @@ export async function createInvoice(formData: FormData) {
   const date = new Date().toISOString().split('T')[0];
 
   await sql`
-    INSERT INTO INVOICES (customer_id, amount, status, date)
+    INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+
+  revalidatePath('dasboard/invoices')
+  redirect('/dashboard/invoices')
 }
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const {customerId, amount, status} = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status')
+  })
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices 
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}`;
+
+  revalidatePath('dasboard/invoices')
+  redirect('/dashboard/invoices')
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`
+    DELETE FROM invoices 
+    WHERE id = ${id}`;
+
+  revalidatePath('dasboard/invoices')
+}
+
